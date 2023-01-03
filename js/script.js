734f5af3d81db40ba376caff75a5527038718b8c
drikadev <adriguti.adriana@hotmail.com>
2023-01-02 22:14:45 -0300

    enviando alterações css e js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const floor3 = document.querySelector('.floor-3')
 
 // Função iniciar jogo
 const start = () => {
+
     document.getElementById('text-start').style.color = '#87CEEB';
 
     mario.src = './img/mario.gif';
@@ -22,6 +23,21 @@ const start = () => {
     koopaShell.classList.add('koopa_shell_animation');
     pipe.classList.add('pipe-animation');
     grass.classList.add('grass-animation');
+
+    // function floorAnimation1() {
+    //     floor1.classList.add('.floor-animation-1');
+    // }
+    // setInterval(floorAnimation1, 1000);
+
+    // function floorAnimation2() {
+    //     floor2.classList.add('.floor-animation-2');
+    // }
+    // setInterval(floorAnimation2, 3000);
+
+    // function floorAnimation3() {
+    //     floor3.classList.add('.floor-animation-3');
+    // }
+    // setInterval(floorAnimation3, 5000);
 
     audioStart.play();
 }
@@ -49,14 +65,14 @@ const checkGameOver = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     const pipePosition = pipe.offsetLeft;
     const grassPosition = grass.offsetLeft;
-    const floorPosition1 = floor1.offsetLeft;
-    const floorPosition2 = floor2.offsetLeft;
-    const floorPosition3 = floor3.offsetLeft;
+    // const floorPosition1 = floor1.offsetLeft;
+    // const floorPosition2 = floor2.offsetLeft;
+    // const floorPosition3 = floor3.offsetLeft;
 
     if (koopaShellPosition <= 125 && koopaShellPosition > 0 && marioPosition < 80) {
         koopaShell.style.animation = 'none';
         //colocar o style.left com template string para o koopashell parar exatamente com os 125px antes do Mario
-        koopaShell.style.left = `${koopaShellPosition}px`
+        koopaShell.style.left = `190px`
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`
@@ -71,24 +87,22 @@ const checkGameOver = setInterval(() => {
         grass.style.animation = 'none';
         grass.style.left = `${grassPosition}px`;
 
-        floor1.style.animation = 'none';
-        floor1.style.left = `${floorPosition1}px`;
+        // floor1.style.animation = 'none';
+        // floor1.style.left = `${floorPosition1}px`;
 
-        floor2.style.animation = 'none';
-        floor2.style.left = `${floorPosition2}px`;
+        // floor2.style.animation = 'none';
+        // floor2.style.left = `${floorPosition2}px`;
 
-        floor3.style.animation = 'none';
-        floor3.style.left = `${floorPosition3}px`;
+        // floor3.style.animation = 'none';
+        // floor3.style.left = `${floorPosition3}px`;
 
         document.getElementById('text-start').style.color = 'black';
         document.getElementById('text-start').innerHTML = 'Game Over';
 
-        function stopAudioStart(){
+        function stopAudioStart() {
             audioStart.pause();
-        } 
-
+        }
         stopAudioStart();
-
         audioGameOver.play();
 
         clearInterval(checkGameOver);
